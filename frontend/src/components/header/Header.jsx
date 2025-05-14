7481fa5f6f5f6b3ba8e871f268838c78f8ba43ab
yugaldhiman <yugaldhiman14@gmail.com>
2025-05-14 11:33:14 +0530

    backend separate folder

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -53,25 +53,26 @@ const Header = () => {
     };
 
     return (
-        <nav className="header">
-            <Link className="logo" to="/home">Study Notes Sharing App</Link>
+        <nav className="studyHeader__container">
+            <Link className="studyHeader__logo" to="/home">Study Notes Sharing App</Link>
 
-            <div className="hamburger" onClick={toggleMenu}>
-                <span className="bar"></span>
-                <span className="bar"></span>
-                <span className="bar"></span>
+            <div className="studyHeader__hamburger" onClick={toggleMenu}>
+                <span className="studyHeader__bar"></span>
+                <span className="studyHeader__bar"></span>
+                <span className="studyHeader__bar"></span>
             </div>
 
-            <div className={`header-box ${menuOpen ? "open" : ""}`}>
+            <div className={`studyHeader__menu ${menuOpen ? "open" : ""}`}>
                 <ul>
-                    <li><Link className="link" to="/">Home</Link></li>
-                    <li><Link className="link" to="/upload">Upload</Link></li>
-                    <li><Link className="link" to="/notes">Notes</Link></li>
-                    <li><Link className="link" to="/profile">Profile</Link></li>
-                    <button className="btnLogout" onClick={handleLogout}>Logout</button>
+                    <li><Link className="studyHeader__link" to="/">Home</Link></li>
+                    <li><Link className="studyHeader__link" to="/upload">Upload</Link></li>
+                    <li><Link className="studyHeader__link" to="/notes">Notes</Link></li>
+                    <li><Link className="studyHeader__link" to="/profile">Profile</Link></li>
+                    <button className="studyHeader__logout" onClick={handleLogout}>Logout</button>
                 </ul>
             </div>
         </nav>
+
     );
 };
 
